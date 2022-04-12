c9430df350ad813e13caa4e677eced38f50689c2
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-12 15:58:07 +0530

    Added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styling/Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="spinner">
+      <div className="loader l1"></div>
+      <div className="loader l2"></div>
+    </div>
+  );
+};
+
+export default Loader;
